de3610192dad43cbbe77df913659478ec06863fc
tajbaba999 <tajbaba153@gmail.com>
2026-06-29 23:39:34 +0530

    fix: mount refresh-token route

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import type MessageResponse from "../interfaces/message-response.js";
 
 import { authenticateToken } from "../middlewares.js";
+import RefreshToken from "./auth/refresh-token.js";
 import Singin from "./auth/signin.js";
 import Signup from "./auth/singup.js";
 import CodingProfile from "./codingprofile/codingprofile.js";
@@ -20,6 +21,7 @@ router.get<object, MessageResponse>("/", (req, res) => {
 router.use("/metrics", Metrics);
 router.use("/signup", Signup);
 router.use("/signin", Singin);
+router.use("/refresh-token", RefreshToken);
 
 // Everything below this line requires a valid  access token.
 router.use(authenticateToken);
